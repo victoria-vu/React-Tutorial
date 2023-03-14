d78a254d8ff51ef25d14d25bda9f881226e21e28
Victoria Vu <victoriamvvu@gmail.com>
2023-03-14 16:27:51 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component }  from 'react';
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+
+export default function Board() {
+  return (
+    <React.Fragment>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </React.Fragment>
+  );
+}
