59b99cf3a7ff58dbdcc4ba07fd1cc244edcdf716
Victoria Vu <victoriamvvu@gmail.com>
2023-03-15 21:30:25 -0700

    Implement turns for two players

--- src/App.js
+++ src/App.js
@@ -10,14 +10,23 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [ xIsNext, setXIsNext ] = useState(true);
   // Use state to record clicked squares
   // Array(9).fill(null) creates an array with nine elements and sets each of them to null.
   const [ squares, setSquares ] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice()
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
